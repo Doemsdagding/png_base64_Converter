b1bb6d684a9ff03e15b0e04a27cae324e0f626bf
Doemsdagding <164404639+Doemsdagding@users.noreply.github.com>
2025-03-03 15:07:35 +0100

    Update packager.py
    fixed the select crash

--- packager.py
+++ packager.py
@@ -6,8 +6,15 @@ PyInstaller.__main__.run([
     '--windowed',
     '--onefile',
     '--add-data', 'base64convert\\base.kv:.',
+    '--add-data', 'base64convert\\madimione-regular.otf:.',
     '--hidden-import', 'pkg_resources.extern.packaging.version',
     '--hidden-import', 'pkg_resources.extern.packaging.specifiers', 
     '--hidden-import', 'pkg_resources.extern.packaging.requirements',
+    '--hidden-import', 'win32timezone',
+    '--hidden-import', 'pywin32',
+    '--hidden-import', 'PIL',
+    '--hidden-import', 'PIL._imagingtk',
+    '--hidden-import', 'PIL._imaging',
+    '--hidden-import', 'PIL._webp',
     'base64convert\\base.py'
 ])
